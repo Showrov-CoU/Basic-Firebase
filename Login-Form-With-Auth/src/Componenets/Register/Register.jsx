112948a05494404593e1003a283d73ca79a17a2b
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-10-02 07:00:58 +0600

    added login form for registered users

--- Login-Form-With-Auth/src/Componenets/Register/Register.jsx
+++ Login-Form-With-Auth/src/Componenets/Register/Register.jsx
@@ -13,15 +13,18 @@ const Register = () => {
     console.log("ok");
     const email = e.target.email.value;
     const pass = e.target.password.value;
-    console.log(email, pass);
+    const checkBox = e.target.terms.checked;
+    console.log(email, pass, checkBox);
     setRegisterError("");
     setSuccess("");
     if (pass.length < 6) {
       setRegisterError("Password should be at least 6 charecter");
       return;
-    }
-    if (!/[A-Z]/.test(pass)) {
+    } else if (!/[A-Z]/.test(pass)) {
       setRegisterError("Your Password should have at least one upper case");
+      return;
+    } else if (!checkBox) {
+      setRegisterError("Please accept our terms and condition");
       return;
     }
     createUserWithEmailAndPassword(auth, email, pass)
@@ -37,7 +40,9 @@ const Register = () => {
   return (
     <div className="w-full h-[78vh] border-y-2">
       <div className="flex flex-col justify-center items-center gap-4 h-full">
-        <h1 className="text-4xl font-bold text-[#d926a9] py-2">Please Register</h1>
+        <h1 className="text-4xl font-bold text-[#d926a9] py-2">
+          Please Register
+        </h1>
         <form onSubmit={handleSubmit} className="space-y-3 w-[40%]">
           <input
             type="email"
@@ -61,7 +66,10 @@ const Register = () => {
               {showPass ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}
             </span>
           </div>
-          <input type="checkbox" name="" id="" />
+          <input type="checkbox" name="terms" id="terms" />
+          <label className="ml-3" htmlFor="terms">
+            Accept our <a href="">Terms and Condition</a>
+          </label>
           <input
             type="submit"
             value="Submit"
